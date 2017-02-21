64d38115cd98c0accc9e863616b9587b2845f603
byteporter <james@byteporter.com>
2017-02-20 21:33:10 -0500

    added static resources

--- images/resume/resume.go
+++ images/resume/resume.go
@@ -4,12 +4,33 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"fmt"
 
 	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
 )
+
+type justFilesFilesystem struct {
+	fs http.FileSystem
+}
+
+func (fs justFilesFilesystem) Open(name string) (http.File, error) {
+	f, err := fs.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return neuteredReaddirFile{f}, nil
+}
+
+type neuteredReaddirFile struct {
+	http.File
+}
+
+func (f neuteredReaddirFile) Readdir(count int) ([]os.FileInfo, error) {
+	return nil, nil
+}
 
 type Resume struct {
 	Body string `json:"body,omitempty`
@@ -24,5 +45,9 @@ func GetResumeEndpoint(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/resume", GetResumeEndpoint).Methods("GET")
+	router.PathPrefix("/resources/").Handler(http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+	http.Handle("/resources/", router)
 	log.Fatal(http.ListenAndServe(":8080", router))
+	// fs := justFilesFilesystem{http.Dir("resources/")}
+	// router.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(fs)))
 }
